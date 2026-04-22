c3a15b3542d9ee6a1ea05fcb42fabefec6634b8b
wuzehv <wuzehui.1024@gmai.com>
2026-04-22 11:01:56 +0800

    neo

--- lib/common/shared.dart
+++ lib/common/shared.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 typedef FromJson<T> = T Function(Map<String, dynamic> json);
 typedef ToJson<T> = Map<String, dynamic> Function(T object);
@@ -9,32 +9,35 @@ class ObjectStore<T> {
   final FromJson<T> fromJson;
   final ToJson<T> toJson;
 
-  static const _storage = FlutterSecureStorage();
-
   ObjectStore({required this.key, required this.fromJson, required this.toJson});
 
+  Future<SharedPreferences> get _prefs async => await SharedPreferences.getInstance();
+
   Future<void> save(String id, T obj) async {
-    final String? jsonString = await _storage.read(key: key);
+    final prefs = await _prefs;
+    final String? jsonString = prefs.getString(key);
     Map<String, dynamic> data = jsonString != null ? json.decode(jsonString) : {};
 
     data[id] = toJson(obj);
 
-    await _storage.write(key: key, value: json.encode(data));
+    await prefs.setString(key, json.encode(data));
   }
 
   Future<List<T>> list() async {
-    final String? jsonString = await _storage.read(key: key);
+    final prefs = await _prefs;
+    final String? jsonString = prefs.getString(key);
     final Map<String, dynamic> m = jsonString != null ? json.decode(jsonString) : {};
 
     return m.values.map<T>((e) => fromJson(Map<String, dynamic>.from(e))).toList();
   }
 
   Future<void> remove(String id) async {
-    final String? jsonString = await _storage.read(key: key);
+    final prefs = await _prefs;
+    final String? jsonString = prefs.getString(key);
     if (jsonString != null) {
       final Map<String, dynamic> data = json.decode(jsonString);
       data.remove(id);
-      await _storage.write(key: key, value: json.encode(data));
+      await prefs.setString(key, json.encode(data));
     }
   }
 }
